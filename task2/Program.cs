b0f0d21de4a5b4bd1db09f6ee15f2b7e7d6a38a3
Kolcman <m.kolcman@yandex.ru>
2024-02-12 12:50:11 +0300

    Lesson4:Task2

---
+++ task2/Program.cs
@@ -0,0 +1,16 @@
+﻿// Task2
+
+System.Console.WriteLine("Введите длину массива");
+int N = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[N];
+for (int i = 0; i < array.Length ; i++)
+{
+    array[i] = i + 1;
+}
+for (int i = 0; i < array.Length; i++)
+{
+    System.Console.Write($"{array[i]} ");
+}
+
+
+

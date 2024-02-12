f378162f9906cdc7e07982a599d70a1556144b9f
Kolcman <m.kolcman@yandex.ru>
2024-02-12 17:08:50 +0300

    Lesson4:task4

---
+++ task4/Program.cs
@@ -0,0 +1,11 @@
+﻿int[] array = {5,1117,67,75,18,8,154,22,12,43};
+int max = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > max){
+        max = array[i];
+    }
+}
+
+System.Console.WriteLine($"Максимальная гиря: {max}");

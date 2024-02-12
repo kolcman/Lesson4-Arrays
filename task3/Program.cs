cba1063af10a42bedb1890a5a03af66f14e79b4a
Kolcman <m.kolcman@yandex.ru>
2024-02-12 12:51:51 +0300

    Lesson4:Task3

---
+++ task3/Program.cs
@@ -0,0 +1,13 @@
+﻿// Task2:2
+
+int[] array = new int[10];
+for (int i = 0; i < array.Length; i++)
+{
+    System.Console.WriteLine("Введите число");
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
+for (int i = 0; i < array.Length; i++)
+{
+    if(array[i] % 2 == 0)
+    System.Console.Write(array[i]);
+}

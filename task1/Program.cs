45a41ac5c5f89c3f2637063e40935189501b2e7a
Kolcman <m.kolcman@yandex.ru>
2024-02-12 12:02:04 +0300

    Lesson4:Task1

---
+++ task1/Program.cs
@@ -0,0 +1,16 @@
+﻿// Arrays - Массивы
+
+int[] arr = new int[5];
+arr[0] = 5;
+arr[1] = 7;
+arr[2] = 3;
+arr[3] = 87;
+arr[4] = 2;
+
+for (int i = 0; i < 5; i++)
+{
+    System.Console.WriteLine(arr[i]);
+}
+
+int[] arr2 = new int[5] { 4, 5, 3, 7, 35 };
+int[] arr3 = { 3, 7, 8, 44, 12 };
